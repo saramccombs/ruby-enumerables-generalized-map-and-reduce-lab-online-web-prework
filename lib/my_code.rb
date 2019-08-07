b909fc41ea21576e075786f02ddd67cfdc902e11
Sara McCombs <thesaramccombs@gmail.com>
2019-08-07 03:26:22 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# Your Code Here
+def map (array)
+  new = []
+  index = 0 
+  while index < array.length do
+    new.push(yield(array[index]))
+    index += 1 
+  end
+  return new
+end
+
+def reduce(array, stating_value*)
+  
+  if starting_value
+    total = starting_value
+    index = 0
+  else
+    total = array[0]
+    index += 1 
+  end
+  
+  while index < array.length do
+    total = yield(total, array[index])
+    index += 1 
+  end
+  
+  return total 
+end
