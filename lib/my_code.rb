a073a20176a9e1b902be6421afb2edb116968409
Sara McCombs <thesaramccombs@gmail.com>
2019-08-07 01:34:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# Your Code Here
+def map (array)
+  new = []
+  index = 0 
+  while index < array.length do
+    new.push(yield(array[index]))
+    index += 1 
+  end
+  return new
+end
+
+def reduce(array, stating_value = 0)
+  total = 0 
+  
+end
